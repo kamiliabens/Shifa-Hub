407d270207b0fe9e0673a482fb67c1699ddf0869
ChWIssu <w.chahed.inf@lagh-univ.dz>
2026-04-22 14:11:46 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 $conn = mysqli_connect("localhost", "root", "", "shifa_db");
 
 if (!$conn) {
